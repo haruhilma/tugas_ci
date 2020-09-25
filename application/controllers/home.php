1979adcbf29a41ef4765ffc77af3ba68fbf5d1b7
Allexandr0 <46394363+Allexandr0@users.noreply.github.com>
2020-09-25 09:14:55 -0700

    photo

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,11 +8,11 @@ class Home extends CI_Controller {
 		
 		$mahasiswa = array(
 
-			 array('nama' =>  'Faris Eko N', 'nim' => '1614321021','Jurusan' => 'Teknik Informatika','kelas' => 'kelas D' ),
-			  array('nama' =>  'Mukamat Rendi S', 'nim' => '1614321046','Jurusan' => 'Teknik Informatika','kelas' => 'kelas D' ),
-				array('nama' =>  'Noviyanti', 'nim' => '1614321015','Jurusan' => 'Teknik Informatika','kelas' => 'kelas D' ),
-				  array('nama' =>  'Subaris', 'nim' => '1614321008','Jurusan' => 'Teknik Informatika','kelas' => 'kelas D' ),
-				   array('nama' =>  'Allexandro Billy S', 'nim' => '1614321059','Jurusan' => 'Teknik Informatika','kelas' => 'kelas D' )
+			 array('nama' =>  'Faris Eko N', 'nim' => '1614321021','jurusan' => 'Teknik Informatika','kelas' => 'kelas D' ),
+			  array('nama' =>  'Mukamat Rendi S', 'nim' => '1614321046','photo' => 'assets/imj/Rendi.jpeg','jurusan' => 'Teknik Informatika','kelas' => 'kelas D' ),
+				array('nama' =>  'Noviyanti', 'nim' => '1614321015','jurusan' => 'Teknik Informatika','kelas' => 'kelas D' ),
+				  array('nama' =>  'Subaris', 'nim' => '1614321008','photo' => 'assets/imj/Rendi.jpeg','jurusan' => 'Teknik Informatika','kelas' => 'kelas D' ),
+				   array('nama' =>  'Allexandro Billy S', 'nim' => '1614321059','jurusan' => 'Teknik Informatika','kelas' => 'kelas D' )
 
 				);
 
