386af5dfd41f63113a849de6bdc013b339149a29
Allexandr0 <46394363+Allexandr0@users.noreply.github.com>
2020-09-25 07:56:10 -0700

    Update home.php

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-	  $this->load->view('welcome_message');
+		$data['query']  = $this->main_model->get_list('mahasiswa',array('perpage'=>10,'offset'=>0,array('by'=>'maha_id','sorting'=>'DESC')));
+		$this->load->view('home');
 	}
+	
 }
