31a5fabf23e19e9545e5a6627182c2aedbb86e5d
Haruhilma <jojobarairuma@gmail.com>
2020-09-25 23:07:04 +0700

    dynamic data mahasiswa view

--- application/views/home.php
+++ application/views/home.php
@@ -1,9 +1,24 @@
 <?php include "default/header.php" ?>
 
-    <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
-        <a class="btn btn-lg btn-primary" href="../components/navbar/" role="button">View navbar docs &raquo;</a>
+<main role="main" class="container">
+
+    <div class="row">
+    
+        <div class="card-group">
+            <?php foreach ($mahasiswa as $key => $v) { ?>
+                <div class="card">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Nama : <?= $v['nama'] ?></h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+
     </div>
+
+</main>
 
 <?php include "default/footer.php" ?>
