e019cf611936c0d0767b2efe99752bc4506df37d
Haruhilma <jojobarairuma@gmail.com>
2020-09-25 23:02:04 +0700

    perubahan view

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,9 @@
-<?php echo '<pre>',print_r($mahasiswa,1),'</pre>'; ?>
+<?php include "default/header.php" ?>
+
+    <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
+        <a class="btn btn-lg btn-primary" href="../components/navbar/" role="button">View navbar docs &raquo;</a>
+    </div>
+
+<?php include "default/footer.php" ?>
