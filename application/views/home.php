0dcefa79a0e00e55380f660b7030b6886ba48aa1
Haruhilma <jojobarairuma@gmail.com>
2020-09-25 23:31:58 +0700

    perubahan view

--- application/views/home.php
+++ application/views/home.php
@@ -2,25 +2,24 @@
 
 <main role="main" class="container">
 
-    <div class="row">
+    <h3>DATA DIRI KELOMPOK 6</h3>
     
-        <div class="card-group">
-            <?php foreach ($mahasiswa as $key => $v) { ?>
-                <div class="card">
-                    <img class="card-img-top" width="100%" src="<?= $v['photo'] ?>" alt="<?= $v['nama'] ?>">
-                    <div class="card-body">
-                    <h5 class="card-title">Nama : <?= $v['nama'] ?></h5>
-                    <p class="card-text">
-                    NIM :  <?= $v['nim'] ?><br/>
-                    Kelas : <?= $v['kelas'] ?><br/>
-                    JURUSAN : <?= $v['jurusan'] ?>
-                    </p>
-                    <p class="card-text"><small class="text-muted">Informasi Mahasiswa</small></p>
-                    </div>
-                </div>
-            <?php } ?>
-        </div>
 
+    <div class="card-group">
+        <?php foreach ($mahasiswa as $key => $v) { ?>
+            <div class="card">
+                <img class="card-img-top" width="100%" src="<?= $v['photo'] ?>" alt="<?= $v['nama'] ?>">
+                <div class="card-body">
+                <h5 class="card-title">Nama : <?= $v['nama'] ?></h5>
+                <p class="card-text">
+                NIM :  <?= $v['nim'] ?><br/>
+                Kelas : <?= $v['kelas'] ?><br/>
+                JURUSAN : <?= $v['jurusan'] ?>
+                </p>
+                <p class="card-text"><small class="text-muted">Informasi Mahasiswa</small></p>
+                </div>
+            </div>
+        <?php } ?>
     </div>
 
 </main>
