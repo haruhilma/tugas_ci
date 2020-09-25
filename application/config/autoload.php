2372df41627f36f8869f9441b048a9b5f9382a90
Haruhilma <jojobarairuma@gmail.com>
2020-09-25 22:08:35 +0700

    update auto load
    hilangkan library tidak perlu

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session','template','form_validation','encrypt','upload','image_lib','cart');
+$autoload['libraries'] = array('database','session','form_validation');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','form','image','text');
+$autoload['helper'] = array('url','form','text');
 
 /*
 | -------------------------------------------------------------------
