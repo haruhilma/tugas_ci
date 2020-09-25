ccdc4c2165805bcc95a4aa883f0554dd1c552972
Allexandr0 <46394363+Allexandr0@users.noreply.github.com>
2020-09-25 08:30:22 -0700

    Update home.php

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,8 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-		$data['query']  = $this->main_model->get_list('mahasiswa',array('perpage'=>10,'offset'=>0,array('by'=>'maha_id','sorting'=>'DESC')));
-		$this->load->view('home');
+		#$data['query']  = $this->main_model->get_list('mahasiswa',array('perpage'=>10,'offset'=>0,array('by'=>'maha_id','sorting'=>'DESC')));
+		
+		$mahasiswa = array("Faris Eko", "Allexandro Billy S", "Rendy");
+		echo "Nama = " . $mahasiswa[0] . ", " . $mahasiswa[1] . " dan " . $mahasiswa[2] . ".";
+
+		#$this->load->view('home');
 	}
 	
 }
